0041ef79513273daf4b91b4343aef5b37663d1af
Rinaldi Utomo <github@rinaldiutomo.com>
2019-02-24 19:25:15 -0500

    Reorganized folder and added bash script

---
+++ src/make.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+if [ -z "$PUID" ]
+then 
+  echo "PUID environment variable is empty. It needs to be set prior to running this script." 
+  exit 1
+fi
+
+if [ -z "$PGID" ]
+then 
+  echo "PGID environment variable is empty. It needs to be set prior to running this script."
+  exit 1
+fi
+
+if [ -z "$TZ" ]
+then
+  echo "TZ environment variable is empty. It needs to be set prior to running this script."
+  exit 1
+fi
+
+if [ -z "$CONFIG_DIR" ]
+then 
+  echo "CONFIG_DIR environment variable is empty. It needs to be set prior to running this script."
+  exit 1
+fi
+
+if [ -z "$DOMAINNAME" ] 
+then
+  echo "DOMAINNAME environment variable is empty. It needs to be set prior to running this script."
+  exit 1
+fi
+
+if [ -z "$CLOUDFLARE_EMAIL" ]
+then 
+  echo "CLOUDFLARE_EMAIL environment variable is empty. It needs to be set prior to running this script."
+  exit 1
+fi
+
+if [ -z "$CLOUDFLARE_EMAIL" ]
+then 
+  echo "CLOUDFLARE_EMAIL environment variable is empty. It needs to be set prior to running this script."
+  exit 1
+fi
+
+folders=( "traefik""portainer/data" "sabnzbd/config" "sabnzbd/downloads" "sabnzbd/incomplete" "sonarr/appdata" "sonarr/downloads" )
+
+for i in ${folders[@]}
+do
+  mkdir -p $i
+done
