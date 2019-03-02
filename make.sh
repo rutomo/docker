9185361198166bae9061b95eacfe14f4e582b63f
Rinaldi Utomo <github@rinaldiutomo.com>
2019-03-01 19:54:42 -0500

    Reorganized files and added traefik.toml

--- make.sh
+++ make.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-if [ -f "$PUID" ]
+if [ -f "media/.env" ]
 then
-  source .env
+  source media/.env
 else
   echo ".env file does not exist. Please create one."
+  exit 1
+fi
 
 if [ -z "$PUID" ]
 then 
@@ -55,4 +57,58 @@ for i in ${folders[@]}
 do
   mkdir -p "$CONFIG_DIR/$i"
   echo $i
+  # if [ "$i" == "traefik" ]
+  # then
+    
+  # fi
 done
+
+cat > "$CONFIG_DIR/traefik/traefik.toml" <<EOL
+debug = false
+
+logLevel = "ERROR"
+defaultEntryPoints = ["https","http"]
+
+[entryPoints]
+  [entryPoints.http]
+  address = ":80"
+    [entryPoints.http.redirect]
+    entryPoint = "https"
+  [entryPoints.https]
+  address = ":443"
+  [entryPoints.https.tls]
+
+[retry]
+
+[docker]
+endpoint = "unix:///var/run/docker.sock"
+domain = "${DOMAINNAME}"
+watch = true
+exposedByDefault = false
+
+[acme]
+email = "${CLOUDFLARE_EMAIL}"
+storage = "acme.json"
+entryPoint = "https"
+#onHostRule = true
+onDemand = false
+
+[acme.dnsChallenge]
+  provider = "cloudflare"
+  delayBeforeCheck = 30
+
+[[acme.domains]]
+  main = "*.${DOMAINNAME}"
+
+[[acme.domains]]
+  main = "${DOMAINNAME}"
+
+[api]
+  entryPoint = "traefik"
+  dashboard = true
+  address = ":8080"
+EOL
+
+cd ./media
+docker network create traefik_proxy
+/usr/bin/docker-compose up -d
